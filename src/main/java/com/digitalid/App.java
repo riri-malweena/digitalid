00e91bc7f2b9cd4d53f7e24e5e7d42d39d04370e
Risindi_Batuvita <risindibatuvita2005@gmail.com>
2026-02-16 21:02:51 +1100

    Sup

--- src/main/java/com/digitalid/App.java
+++ src/main/java/com/digitalid/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Hello from Riri 👑 TEST 123");
+        System.out.println("Hello Sienna");
 ;
     }
 }
