4e860d2c6d6194778cd1462f892e14d39b1043e8
sfruci <s4137994@student.rmit.edu.au>
2026-02-16 21:10:00 +1100

    3rd test

--- src/main/java/com/digitalid/App.java
+++ src/main/java/com/digitalid/App.java
@@ -8,7 +8,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Hello Sienna");
+        System.out.println("Hello from Riri 👑 TEST 123");
+        System.out.println("Hello from Sienna 👑 TEST 456");
+
+           System.out.println("Hellooooo 👑 TEST 789");
+
 ;
     }
 }
